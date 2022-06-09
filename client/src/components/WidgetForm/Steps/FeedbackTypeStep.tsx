48e45dfb22889a58c139dcb93b2c60499bfb737d
Manfrinne <m4nfrinne@gmail.com>
2022-06-09 11:58:01 -0300

    upload front-end aplication

---
+++ client/src/components/WidgetForm/Steps/FeedbackTypeStep.tsx
@@ -0,0 +1,43 @@
+import { FeedbackType, feedbackTypes } from "..";
+import { CloseButton } from "../../CloseButton";
+
+// Preciso avisar o TypeScript que há uma propriedade/função no componente
+interface FeedbackTypeStepProps {
+  onFeedbackTypeChanged: (type: FeedbackType) => void;
+}
+
+export function FeedbackTypeStep({
+  onFeedbackTypeChanged,
+}: FeedbackTypeStepProps) {
+  return (
+    // Conceito de 'Fragment' = <> Conteúdo </>
+    <>
+      <header>
+        <span className="text-xl leading-6">Deixe seu Feedback</span>
+
+        <CloseButton />
+      </header>
+
+      <div className="flex py-8 gap-2 w-full">
+        {Object.entries(feedbackTypes).map(([key, value]) => {
+          return (
+            <button
+              key={key}
+              className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
+              // Comunicação de um componente com outro através de funções 'onFeedbackTypeChanged'
+              onClick={() => onFeedbackTypeChanged(key as FeedbackType)}
+              type="button"
+            >
+              <img
+                className="mx-auto"
+                src={value.image.source}
+                alt={value.image.alt}
+              />
+              <span>{value.title}</span>
+            </button>
+          );
+        })}
+      </div>
+    </>
+  );
+}
---
+++ client/src/components/WidgetForm/index.tsx
@@ -0,0 +1,73 @@
+import bugImageUrl from "../../assets/bug.png";
+import ideaImageUrl from "../../assets/idea.png";
+import thoughImageUrl from "../../assets/thought.png";
+import { useState } from "react";
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+import { FeedbackSuccessStep } from "./Steps/FeedbackSuccessStep";
+
+export type FeedbackType = keyof typeof feedbackTypes;
+export const feedbackTypes = {
+  BUG: {
+    title: "Problema",
+    image: {
+      source: bugImageUrl,
+      alt: "Imagem de um inseto",
+    },
+  },
+  IDEA: {
+    title: "Ideia",
+    image: {
+      source: ideaImageUrl,
+      alt: "Imagem de uma lâmpada",
+    },
+  },
+  OTHER: {
+    title: "Outro",
+    image: {
+      source: thoughImageUrl,
+      alt: "Imagem de um balão de pensamento",
+    },
+  },
+};
+
+export function WidgetForm() {
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+  const [feedbackSent, setFeedbackSent] = useState(false);
+
+  function handleRestartFeedback() {
+    setFeedbackSent(false);
+    setFeedbackType(null);
+  }
+
+  return (
+    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+      {feedbackSent ? (
+        <FeedbackSuccessStep onFeedbackRestartRequest={handleRestartFeedback} />
+      ) : (
+        <>
+          {!feedbackType ? (
+            // Enviar função 'onFeedbackTypeChanged' como uma propriedade de um componente
+            <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+          ) : (
+            <FeedbackContentStep
+              feedbackType={feedbackType}
+              onFeedbackRestartRequest={handleRestartFeedback}
+              onFeedbackSent={() => setFeedbackSent(true)}
+            />
+          )}
+        </>
+      )}
+
+      <footer className="text-xs text-neutral-400">
+        Feito com ♥ pela{" "}
+        <a
+          className="underline underline-offset-2"
+          href="https://rocketseat.com.br"
+        >
+          Rocketseat
+        </a>
+      </footer>
+    </div>
+  );
+}
